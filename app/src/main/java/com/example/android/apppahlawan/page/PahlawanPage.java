3d307e78b10703334b7e51192af2a63bd8b6878b
Leo Nadeak <lnadeak97@gmail.com>
2017-12-07 15:14:28 +0700

    Detail Pahlawan

--- app/src/main/java/com/example/android/apppahlawan/page/PahlawanPage.java
+++ app/src/main/java/com/example/android/apppahlawan/page/PahlawanPage.java
@@ -1,11 +1,16 @@
 package com.example.android.apppahlawan.page;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.android.apppahlawan.R;
 import com.example.android.apppahlawan.controller.PahlawanAdapter;
+import com.example.android.apppahlawan.detail_pahlawan;
 import com.example.android.apppahlawan.entity.Pahlawan;
 
 import java.util.ArrayList;
@@ -15,7 +20,7 @@ import java.util.GregorianCalendar;
 
 
 public class PahlawanPage extends AppCompatActivity {
-
+    private ImageView mPahlawanView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +35,16 @@ public class PahlawanPage extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list_pahlawan);
         listView.setAdapter(adapterPahlawan);
 
+        mPahlawanView = (ImageView) findViewById(R.id.foto_pahlawan_view);
+        mPahlawanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), detail_pahlawan.class);
+                TextView namaPahlawan = (TextView) findViewById(R.id.nama_pahlawan_view);
+                i.putExtra("nama_pahlawan", namaPahlawan.getText().toString());
+
+                startActivity(i);
+            }
+        });
     }
 }
