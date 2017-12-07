d23d29531f4a0e2142913c455c653d82a07e33a6
rizkihalasan <rizki_halasan@yahoo.com>
2017-12-07 10:46:26 +0700

    tombol

--- app/src/main/java/com/example/android/apppahlawan/MainActivity.java
+++ app/src/main/java/com/example/android/apppahlawan/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.apppahlawan;
 
 import android.app.WallpaperManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -16,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView mywallpaper = (ImageView) findViewById(R.id.cover_pahlawan);
         mywallpaper.setImageResource(R.drawable.cover_pahlawan);
+
+        Button pahlawanButton = (Button) findViewById(R.id.pahlawan);
+
+        pahlawanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pahlawanIntent = new Intent(MainActivity.this, Pahlawan.class);
+                startActivity(pahlawanIntent);
+            }
+        });
+
+        Button quotesButton = (Button) findViewById(R.id.quotes);
+        quotesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent quotesIntent = new Intent(MainActivity.this, Quotes.class);
+                startActivity(quotesIntent);
+            }
+        });
     }
 }
